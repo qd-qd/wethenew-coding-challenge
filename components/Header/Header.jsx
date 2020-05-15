987ef6abbcf7a40c0326a586ab9fcaf87d8a7163
jonathangiamp <jonathan.giamporcaro@gmail.com>
2020-05-15 20:37:36 +0200

    [dev]front: create basic header

---
+++ components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+const Header = () => (
+  <header className={styles.header}>
+    <div className={styles['header-content']}>
+      <img src="/logo.png" alt="logo" className={styles.logo} />
+    </div>
+  </header>
+);
+
+export default Header;
