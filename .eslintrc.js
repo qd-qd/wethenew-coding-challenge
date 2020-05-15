02ce6c641c46e7a879f1833f724bf6d7a50cdee9
jonathangiamp <jonathan.giamporcaro@gmail.com>
2020-05-15 15:09:50 +0200

    [dev]quality: configure prettier
    Added some arbitrary rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,22 @@ module.exports = {
     node: true,
   },
   parser: 'babel-eslint',
-  plugins: ['react'],
+  plugins: ['react', 'prettier'],
   extends: [
+    'airbnb',
     'eslint:recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:jsx-a11y/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'airbnb',
+    'plugin:prettier/recommended',
   ],
   rules: {
     // @DEV: NextJs does not require you to import React into each component.
     'react/react-in-jsx-scope': 'off',
     'react/display-name': 1,
+    'prettier/prettier': 'error',
   },
   settings: {
     react: {
