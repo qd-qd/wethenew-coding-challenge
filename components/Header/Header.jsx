ccd57ce49d4acc00560dd8f1aad42e759aedf35a
jonathangiamp <jonathan.giamporcaro@gmail.com>
2020-05-17 00:16:32 +0200

    [design]front: update header to allow navigation

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import styles from './Header.module.css';
 
 const Header = () => (
   <header className={styles.header} data-testid="header">
     <div className={styles['header-content']}>
-      <img src="/logo.png" alt="logo" className={styles.logo} />
+      <Link href="/">
+        <img src="/logo.png" alt="logo" className={styles.logo} />
+      </Link>
     </div>
   </header>
 );
