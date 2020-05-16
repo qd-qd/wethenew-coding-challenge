e8a264b9d249fb473bd8fb6ee727d9b246189fc0
jonathangiamp <jonathan.giamporcaro@gmail.com>
2020-05-16 19:18:19 +0200

    [design]front: improve header design
    In addition to that I improved Header test by adding snapshot

--- components/Header/Header.test.js
+++ components/Header/Header.test.js
@@ -2,10 +2,16 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Header from './Header';
 
-describe('Header testing', () => {
+describe('Header', () => {
   test('displays greeting', () => {
     render(<Header />);
     expect(screen.getByRole('img')).toHaveAttribute('alt');
     expect(screen.getByRole('img')).toHaveAttribute('src');
+  });
+
+  test("doesn't break snapshot", () => {
+    const { container } = render(<Header />);
+
+    expect(container).toMatchSnapshot();
   });
 });
